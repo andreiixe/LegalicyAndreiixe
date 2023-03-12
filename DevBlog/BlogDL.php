0e9bb8a9eda8808170084a3d2a3363cd7180a78e
Andrei Alexandru <97725399+andreiixe@users.noreply.github.com>
2023-03-12 21:27:28 +0200

    Build 311.5.1 (Dev Blog final release)

---
+++ DevBlog/BlogDL.php
@@ -0,0 +1,42 @@
+<table border="0" cellspacing="0" cellpadding="3" width="415">
+                            <tbody>
+                                <tr>
+                                    <td colspan="2">
+                                        <font face="geneva, arial">
+                                        Here I will write a documentation for every thing that I solve or code to help other people to advance and help any community.
+                                        </font>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td width="96" align="left" valign="bottom">&nbsp;</td>
+                                    <td width="319" align="left" valign="top">
+                                        <font face="geneva,arial" size="-1">
+                                            <br>
+                                            <font color="#FF0000">
+                                                <b>&gt;</b>
+                                                <a href="../DevBlog/ChromXP.html">Chromium newer versions on Windows XP</a>
+                                                <font style="font-size:13px;">(12 March 2023)</font>&nbsp;<img src="../src/imgeo/new3.gif">
+                                            </font>
+                                        </font>
+                                        <p><br>
+                                            <table width="525" border="0" cellpadding="4" cellspacing="2">
+                                            <tbody><tr>
+                                                <td colspan="2">
+                                                <hr size="1">
+                                                </td>
+                                            </tr>
+                                            
+                                            <tr>
+                                                <td width="450" rowspan="3">&nbsp;</td>
+                                                <td valign="bottom" bgcolor="#cccccc" align="center">
+                                                <font face="geneva, arial" size="-1">
+                                                    <a href="../index.html">Home</a>
+                                                    </font>
+                                                </td>
+                                            </tr>
+                                            </tbody></table>
+                                        </p>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
--- DevBlog/Blogdev.php
+++ DevBlog/Blogdev.php
@@ -1,7 +1,7 @@
 <!-- Working progress -->
 <html>
     <head>
-        <title>Welcome to Legalicy Andreiixe</title>
+        <title>Legalicy Andreiixe | Dev Blog</title>
     </head>
     <body topmargin="5" leftmargin="5" bgcolor="#ffffff" link="#0000CC" vlink="#660099" alink="#cccccc">
         <table border="0" width="590" cellpadding="0" cellspacing="0">
@@ -10,18 +10,18 @@
                     <!--Prima coloana-->
                     <!--NavBar-->
                     <td width="140">
-                        <img src="src/images/LegalicyLogo.gif" width="138" height="34" alt="Legalicy Andreiixe" border="0">
+                        <img src="../src/images/LegalicyLogo.gif" width="138" height="34" alt="Legalicy Andreiixe" border="0">
                         <br>
-                        <a href="about.html">
-                            <img src="src/Bar/about.gif" width="138" height="36" alt="About" border="0">
+                        <a href="../about.html">
+                            <img src="../src/Bar/about.gif" width="138" height="36" alt="About" border="0">
                         </a>
                         <br>
-                        <a href="blog.html">
-                            <img src="src/Bar/Blog.gif" width="138" height="36" alt="Blog" border="0">
+                        <a href="../blog.html">
+                            <img src="../src/Bar/Blog.gif" width="138" height="36" alt="Blog" border="0">
                         </a>
                         <br>
-                        <a href="link.html">
-                            <img src="src/Bar/Links.gif" width="138" height="36" alt="Links" border="0">
+                        <a href="../link.html">
+                            <img src="../src/Bar/Links.gif" width="138" height="36" alt="Links" border="0">
                         </a>
                         <br>
                         <br>
@@ -39,13 +39,16 @@
                                 <a href="http://sleepkindle.rf.gd/">Andreiixe Kindle</a>
                                 <font size="+1">&nbsp;</font>
                                 <br>
-                                <a href="Support/IBMThinkR51e.html">Support</a>
+                                <a href="../Support/IBMThinkR51e.html">Support</a>
                                 <font size="+1">&nbsp;</font>
                                 <br>
-                                <a href="feedback.html">Feedback</a>
+                                <a href="../feedback.html">Feedback</a>
                                 <font size="+1">&nbsp;</font>
                                 <br>
-                                <a href="linkus.html">Link to Us</a>
+                                <a href="../linkus.html">Link to Us</a>
+                                <font size="+1">&nbsp;</font>
+                                <br>
+                                <a href="src/txt/Builds.txt">Updates</a>
                                 <font size="+1">&nbsp;</font>
                                 <br>
                             </font>
@@ -61,7 +64,7 @@
                         <!--/Host-->
                         <!--Msie-->
                         <p>
-                            <img src="src/Imagesads/IE4.gif" width="108" height="31" border="0">
+                            <img src="../src/Imagesads/IE4.gif" width="108" height="31" border="0">
                         </p>
                         <!--/msie-->
                         <!--copyright (I added more because it looks cool)-->
@@ -74,17 +77,17 @@
                         <!--copyright-->
                     </td>
                     <td width="4" valign="bottom">
-                        <img src="src/images/punctsalv.gif" width="4" height="1">
+                        <img src="../src/images/punctsalv.gif" width="4" height="1">
                     </td>
                     <td bgcolor="#666666" width="1" valign="bottom">
-                        <img src="src/images/punctsalv.gif" width="1" height="1">
+                        <img src="../src/images/punctsalv.gif" width="1" height="1">
                     </td>
                     <td width="5" valign="bottom">
-                        <img src="src/images/punctsalv.gif" width="5" height="1">
+                        <img src="../src/images/punctsalv.gif" width="5" height="1">
                     </td>
                     <!--/navbar-->
                     <!-- include blog -->
-                    <?php include 'Blog/Blogbody.html'; ?>
+                    <?php include 'Blogbody.php'; ?>
                 </tr>
             </tbody>
         </table>
